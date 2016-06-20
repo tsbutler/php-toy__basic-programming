d220c974cf80f78a0eceaf506d2f1fe973bf48d2
Thorin Butler <thorin.butler@gmail.com>
2016-06-20 15:50:37 -0500

    Finished final basic PHP exercise with test passing

--- lib/4_filter_numbers.php
+++ lib/4_filter_numbers.php
@@ -7,9 +7,29 @@
 // Run `bin/check` from the command line to execute the automated tests.
 
 function filterNumbers($arr) {
-  // Your code goes here.
+  $new_arr = array();
+  foreach($arr as $value) {
+    if ($value < 10){
+      array_push($new_arr, $value);
+    }
+  }
+  return $new_arr;
 }
 
 // Write your own "tests" below. Refer to the the examples from exercise #1.
+echo "\nTesting that (30, 5, 9, 10, 11) filters to (5,9). Should return true.\n";
 
+$arr = array(30, 5, 9, 10, 11);
+$result = array(5,9);
+
+if (filterNumbers($arr) == $result) {
+  echo "\nSuccess!";
+}
+else{
+  echo "\nFailed!";
+}
+echo "\n-------------------------------------------------------------------\n";
+
+echo "\n###################################################################\n";
+echo "Tests complete.\n";
 ?>
