5036f19bbf4d29073f9025df1257ddaf53147d36
Thorin Butler <thorin.butler@gmail.com>
2016-06-20 14:12:41 -0500

    second exercise complete with tests passing

--- lib/2_uppercases_all_letters.php
+++ lib/2_uppercases_all_letters.php
@@ -7,9 +7,28 @@
 // Run `bin/check` from the command line to execute the automated tests.
 
 function uppercasesAllLetters($str) {
-  // Your code goes here.
+  $str = strtoupper($str);
+  return $str;
 }
 
 // Write your own "tests" below. Refer to the the examples from exercise #1.
+echo "\nTesting input 'uppercase me' gets converted to 'UPPERCASE ME'.  Should return 'Success!'\n";
+if (uppercasesAllLetters('uppercase me') == 'UPPERCASE ME') {
+  echo "\nSuccess!";
+}
+else{
+  echo "\nFailed!";
+}
+echo "\n-------------------------------------------------------------------\n";
 
+echo "\nTesting input 'UpPeRcAsE mE' gets converted to 'UPPERCASE ME'.  Should return 'Success!'\n";
+if (uppercasesAllLetters('UpPeRcAsE mE') == 'UPPERCASE ME') {
+  echo "\nSuccess!";
+}
+else{
+  echo "\nFailed!";
+}
+echo "\n-------------------------------------------------------------------\n";
+echo "\n###################################################################\n";
+echo "Tests complete.\n";
 ?>
